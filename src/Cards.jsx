2e0bc576d1d5b222d09e20718f64ac85968d81e1
Jared588 <jaredeprice@gmail.com>
2024-02-13 07:21:49 +0200

    Improve formatting

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,62 +3,49 @@ import { useState, useEffect } from 'react';
 import Card from './containers/Card';
 import PropTypes from 'prop-types';
 
-// Fetches pokemon data
-function GetPokemon() {
-  const [pokemonData, setPokemonData] = useState([]);
-
-  useEffect(() => {
-    const fetchPokemon = (id) => {
-      fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setPokemonData((prevData) => {
-            // Replace the existing data with the new data (prevents duplicates)
-            const newData = [...prevData];
-            newData[id - 1] = data;
-            return newData;
-          });
-        })
-        .catch((error) => {
-          console.error('Error fetching Pokemon data:', error);
-        });
-    };
-
-    // Fetch 8 pokemon
-    for (let i = 1; i <= 8; i++) {
-      fetchPokemon(i);
-    }
-  }, []);
-
-  return pokemonData;
-}
-
-// Fisher-Yates shuffle algorithm
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
 // Display logic
 function DisplayCards({ score, setScore }) {
+  const [pokemonData, setPokemonData] = useState([]);
   const [trackedList, setTrackedList] = useState([]);
   const [flip, setFlip] = useState(false);
 
-  let randomArray = shuffleArray([...Array(8).keys()]); // Set inital array between 0-7
-  const data = GetPokemon(); // Get pokemon data
+  // Fetches pokemon data
+  function GetPokemon() {
+    useEffect(() => {
+      const fetchPokemon = (id) => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then((data) => {
+            setPokemonData((prevData) => {
+              // Replace the existing data with the new data (prevents duplicates)
+              const newData = [...prevData];
+              newData[id - 1] = data;
+              return newData;
+            });
+          })
+          .catch((error) => {
+            console.error('Error fetching Pokemon data:', error);
+          });
+      };
 
-  // Track pokemon
+      // Fetch 8 pokemon
+      for (let i = 1; i <= 8; i++) {
+        fetchPokemon(i);
+      }
+    }, []);
+
+    return pokemonData;
+  }
+
+  // Tracks selected pokemon
   function trackPokemon(name) {
+    // Check if the pokemon has been previously selected
     if (!trackedList.includes(name)) {
-      // Check if the pokemon has been previously selected
       setTrackedList((prevData) => {
         return [...prevData, name]; // Log pokemon
       });
@@ -80,6 +67,9 @@ function DisplayCards({ score, setScore }) {
     }
   }
 
+  let randomArray = shuffleArray([...Array(8).keys()]); // Set inital array between 0-7
+  const data = GetPokemon(); // Get pokemon data
+
   return (
     <>
       {data.length > 0 && (
@@ -97,6 +87,15 @@ function DisplayCards({ score, setScore }) {
       )}
     </>
   );
+}
+
+// Fisher-Yates shuffle algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 DisplayCards.propTypes = {
